14bd675edf75ea55ad046b5ecfd2f31090bcdf28
dcribeiro <dcribeiro@ua.pt>
2017-04-06 14:59:10 +0100

    The application is already fully functional

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # save2pdf_embedfonts
 Matlab save2pdf modified to have embedded fonts
+
+ghostscript.m function comes from export_fig
+save2pdf_embed.m is the function to use
+	it is adapted from save2pdf.m, besides what was done previously by save2pdf
+	it saves the image in a pdf format with a random temporary name, unique inside the folder in use (the folder where the final file will be saved)
+	it resaves the image and embeds all the fonts (uses ghostscript for this job)
+		(atention should be given to 3D images -> the 'painters' renderer is being used to force the image to be saved in vectorial format, it might generate issues with 3D images)
+
+- Requirements:
+	Ghostscript must be installed (tested on the 64bit version)
+	
---
+++ save2pdf_embed.m
@@ -0,0 +1,113 @@
+%SAVE2PDF Saves a figure as a properly cropped pdf
+%
+%   save2pdf(pdfFileName,handle,dpi)
+%
+%   - pdfFileName: Destination to write the pdf to.
+%   - handle:  (optional) Handle of the figure to write to a pdf.  If
+%              omitted, the current figure is used.  Note that handles
+%              are typically the figure number.
+%   - dpi: (optional) Integer value of dots per inch (DPI).  Sets
+%          resolution of output pdf.  Note that 150 dpi is the Matlab
+%          default and this function's default, but 600 dpi is typical for
+%          production-quality.
+%
+%   Saves figure as a pdf with margins cropped to match the figure size.
+
+%   (c) Gabe Hoffmann, gabe.hoffmann@gmail.com
+%   Written 8/30/2007
+%   Revised 9/22/2007
+%   Revised 1/14/2007
+%   Diogo Ribeiro, dcribeiro@ua.pt
+%   Revised 02/04/2017
+
+function save2pdf_embed(pdfFilePath,handle,dpi)
+
+% Verify correct number of arguments
+narginchk(0,3);
+
+% If no handle is provided, use the current figure as default
+if nargin<1
+    [fileName,pathName] = uiputfile('*.pdf','Save to PDF file:');
+    if fileName == 0; return; end
+    pdfFilePath = [pathName,fileName];
+end
+if nargin<2
+    handle = gcf;
+end
+if nargin<3
+    dpi = 150;
+end
+
+% get pathname WITH extension
+[basepath,filename,fileext] = fileparts(pdfFilePath);
+if isempty(fileext)
+    fileext = '.pdf';
+end
+pdfFilePath = fullfile(basepath,[filename,fileext]);
+
+if exist(pdfFilePath,'file')
+    % delete first...
+    delete(pdfFilePath);
+end
+
+% generate random unique filename for temporary .pdf file
+tempname = strrep(num2str(rand(1)*10000),'.','t');
+if exist(tempname,'file')
+    % delete first...
+    tempname = strrep(num2str(rand(1)*10000),'.','t');
+end
+pdfTempPath = fullfile(basepath,[tempname,fileext]);
+
+% Backup previous settings
+prePaperType = get(handle,'PaperType');
+prePaperUnits = get(handle,'PaperUnits');
+preUnits = get(handle,'Units');
+prePaperPosition = get(handle,'PaperPosition');
+prePaperSize = get(handle,'PaperSize');
+
+% Make changing paper type possible
+set(handle,'PaperType','<custom>');
+
+% Set units to all be the same
+set(handle,'PaperUnits','inches');
+set(handle,'Units','inches');
+
+% Set the page size and position to match the figure's dimensions
+% paperPosition = get(handle,'PaperPosition');
+position = get(handle,'Position');
+set(handle,'PaperPosition',[0,0,position(3:4)]);
+set(handle,'PaperSize',position(3:4));
+
+% Force the rendered to be 'Painters' 
+% (this is the only one that can be saved in vectorial format)
+% renderer = get(handle,'Renderer');
+% set(handle,'Renderer','Painters');
+
+% Save the pdf (this is the same method used by "saveas")
+print(handle,'-painters','-dpdf',pdfTempPath,sprintf('-r%d',dpi))
+
+% Restore the previous settings
+set(handle,'PaperType',prePaperType);
+set(handle,'PaperUnits',prePaperUnits);
+set(handle,'Units',preUnits);
+set(handle,'PaperPosition',prePaperPosition);
+set(handle,'PaperSize',prePaperSize);
+% set(handle,'Renderer',renderer);
+
+%% Resave .pdf with embeded fonts
+
+cmd_str = ['-q -dSAFER -dNOPLATFONTS -dNOPAUSE -dBATCH -sDEVICE=pdfwrite ', ...
+        '-dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer ', ...
+        '-dMaxSubsetPct=100 -dSubsetFonts=true -dEmbedAllFonts=true ',...
+        '-sOutputFile=', pdfFilePath, ' -f ', pdfTempPath];
+try
+    ghostscript(cmd_str);
+catch me
+    % delete temp...
+    delete(pdfTempPath);
+    throw(me);
+end
+% delete temp...
+delete(pdfTempPath);
+
+end
